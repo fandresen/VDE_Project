2b5bcb7937883e36abd0e1898f23c66280c8142d
fandresena <fandresenaantokiniaina1@gmail.com>
2024-05-23 19:09:02 +0200

    Hariva 23/05/202

--- src/components/Admin/AddNewUser/AddNewUser.tsx
+++ src/components/Admin/AddNewUser/AddNewUser.tsx
@@ -1,11 +1,34 @@
+// import { useState } from "react";
+// import Modal from "./PopUp";
 import FormulaireInsertionUser from "./formulaire";
 
 
 export default function AddNewUser() {
+
+  // const [showModal, setShowModal] = useState(false);
+
+  // const handleOpenModal = () => {
+  //   setShowModal(true);
+  // };
+
+  // const handleCloseModal = () => {
+  //   setShowModal(false);
+  // };
+
   return (
     <>
       <h1 className=" text-4xl xl:text-6xl text-primary font-bold text-center mt-[2vh]">Créer un nouveau Utilisateur</h1>
       <FormulaireInsertionUser/>
+
+      {/* <div className="flex items-center justify-center">
+      <button
+        onClick={handleOpenModal}
+        className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700"
+      >
+        Open Modal
+      </button>
+      <Modal show={showModal} onClose={handleCloseModal} />
+    </div> */}
     </>
   )
 }
--- src/components/Admin/AddNewUser/formulaire.tsx
+++ src/components/Admin/AddNewUser/formulaire.tsx
@@ -1,18 +1,107 @@
-import {ChangeEvent, useState } from "react"
+import {useState } from "react"
 import './formulaire.css'
+import axios from "axios"
+import { SelectChangeEvent } from "@mui/material";
+
+const emailRegex = "/^[^\s@]+@[^\s@]+\.[^\s@]+$/";
+
+interface dataFormT {
+    first_name : string,
+    last_name : string,
+    email : string,
+    password : string,
+    role : string
+}
 
 
 export default function FormulaireInsertionUser() {
+
+    const [dataForm,setDataForm] = useState<dataFormT>()
+    const [samePsswd,setSamePsswd]=useState<boolean>(true)
+    const [emailValid,setemailValid]=useState<boolean>(true)
+
+    const [formValues, setFormValues] = useState({
+        email: '',
+        nom: '',
+        prenom: '',
+        psswd: '',
+        poste:'',
+        conf_psswd: '',
+      });
+    
+      const [formValid, setFormValid] = useState(false);
+    
+      const handleInputChange = (name: string, value: string) => {
+    setFormValues((prevValues) => {
+      const newValues = { ...prevValues, [name]: value };
+
+      // Vérifiez si tous les champs ne sont pas vides
+      const isValid = Object.values(newValues).every((val) => val.trim() !== '');
+      setSamePsswd(true);
+      
+      if (newValues.conf_psswd !=="") {
+        if (newValues.psswd===newValues.conf_psswd) {
+        setFormValid(isValid); 
+      }
+      else {
+        setSamePsswd(false)
+        setFormValid(false)
+      }    
+      }
+      return newValues;
+    });
+  };
+
+  const handleSelectChange = (e:SelectChangeEvent)=>{
+      setFormValues((prevValues) => {
+      const newValues = { ...prevValues, [e.target.name]: e.target.value };
+
+       const isValid = Object.values(newValues).every((val) => val.trim() !== '');
+       setFormValid(isValid)
+      return newValues;
+    });
+    
+  }                       
+    
+      const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        // Soumettez le formulaire uniquement si il est valide
+        if (formValid) {
+          // Faites quelque chose avec les données du formulaire
+          // const data = {...dataForm}
+          // data.email = formValues.email;
+          // data.first_name = formValues.nom;
+          // data.last_name = formValues.prenom;
+          // data.password = formValues.conf_psswd;
+          // data.role = formValues.poste;
+          setDataForm({
+          email:formValues.email,
+          first_name:formValues.nom,
+          last_name:formValues.prenom,
+          password:formValues.conf_psswd,
+          role:formValues.poste
+        })
+
+        setTimeout(()=>console.log(dataForm),5000)
+        
+          
+        } else {
+          console.log('Form not submitted. Please fill all fields.');
+        }
+      };
+ 
+
     return (
       <>
         <div className="h-[80vh] xl:h-[70vh] w-[80vw] bg-white mx-auto mt-10 pt-[3vh] rounded-3xl shadow-lg shadow-black90">
-            <form className="">
+            <form className="" onSubmit={handleSubmit} name="createuserForm">
 
                 <div className="flex gap-24 justify-center">
-                    <EmailInput placeholder={'email'}/>
+                    <InputC placeholder={'email'} type={'email'} name={'email'} erreur={!emailValid} onChange={handleInputChange} value={formValues.email} />
 
                     <div className="relative mt-10">
-                            <select className="text-2xl text-primary bg-white font-medium p-4 w-[30vw] border-2 border-primary rounded-lg hover:cursor-pointer" aria-placeholder="poste">
+                            <select className="text-2xl text-primary bg-white font-medium p-4 w-[30vw] border-2 border-primary rounded-lg hover:cursor-pointer" name="poste" onChange={handleSelectChange}>
                                 <option className="text-primary hover:cursor-pointer mt-5" value="admin"> Admin</option>
                                 <option className="text-secondary hover:cursor-pointer mt-5" value="superviseur">Superviseur</option>
                                 <option className="text-extracteur hover:cursor-pointer mt-5" value="extracteur">Extracteur</option>
@@ -23,15 +112,16 @@ export default function FormulaireInsertionUser() {
 
                 </div>    
                 <div className="flex gap-24 justify-center mt-[2vh]">
-                    <TextInput  placeholder={'Nom'}/>
-                    <TextInput placeholder={'Prenom'}/>
+                    <InputC placeholder={'Nom'} type={'text'} name={'nom'} onChange={handleInputChange} value={formValues.nom}/>
+                    <InputC placeholder={'Prenom'} type={'text'} name={'prenom'} onChange={handleInputChange} value={formValues.prenom}/>
                 </div>       
-                <div className="flex gap-24 justify-center mt-[2vh]">
-                    <PswdInput  placeholder={'Mot de passe'}/>
-                    <PswdInput  placeholder={'Confirmer mot de passe'}/>
+                <div className="flex gap-24 justify-center mt-[2vh] relative">
+                    <InputC placeholder={'Mot de passe'} type={'password'} name={'psswd'} erreur={!samePsswd} onChange={handleInputChange} value={formValues.psswd}/>
+                    <InputC placeholder={'Confirmer mot de passe'} type={'password'} name={'conf_psswd'} erreur={!samePsswd} onChange={handleInputChange} value={formValues.conf_psswd}/>
                 </div>       
+                <h1 className={`text-red-600 text-xl ml-[8vw] mt-2 ${samePsswd?'hidden':''}`}>Mot depasse et Confirmer mot depasse ne se ressemble pas</h1>
                 <div className="mt-16 text-center">
-                    <button className="text-4xl font-bold py-[2vh] px-36 text-primary border-[5px] border-secondary rounded-2xl hover:bg-sky-100">Créer</button>
+                    <button className="text-4xl font-bold py-[2vh] px-36 text-primary border-[5px] border-secondary rounded-2xl hover:bg-sky-100 disabled:text-gray-500  disabled:border-gray-400 disabled:hover:cursor-not-allowed"disabled={!formValid}>Créer</button>
                 </div>
          
             </form>
@@ -40,87 +130,18 @@ export default function FormulaireInsertionUser() {
     )
   }
 
-  const TextInput = ({placeholder}:any)=>{
-    const [focus,setFocus] = useState<boolean>(false)
-    const [inputValue,SetInputValue] = useState<string>('')
-
-
-    const handleChange =(e:ChangeEvent<HTMLInputElement>)=>{
-        SetInputValue(e.target.value)
-    }
-    const handleFocus =()=>{
-        setFocus(true)  
-    }
-    const handleBlur=()=>{
-        if (inputValue==="") {
-            setFocus(false)
-        }
-        else console.log('error')
-    }
-    const clickLabel = ()=>{
-        if (focus && inputValue==="") {
-            setFocus(false)
-        }
-        else {
-            setFocus(true)
-        }
-    }
-
-    return (
-        <>
-             <div className="relative mt-10">
-                <h3 className={`text-primary absolute left-5 ${focus? 'top-[-1.5vh] text-2xl' : ' text-3xl top-3'} bg-white`} id="label" onClick={clickLabel}>{placeholder}</h3>
-                <input type="text" className="text-2xl p-4 w-[30vw] border-2 border-primary rounded-lg" onFocus={handleFocus} onBlur={handleBlur} value={inputValue} onChange={handleChange}/>
-            </div>
-        </>
-    )
+  interface CustomInputProps {
+    name: string;
+    placeholder: string;
+    value: string;
+    type: string;
+    erreur: boolean; 
+    onChange: (name: string, value: string) => void;
   }
+  const InputC = ({placeholder,name,type,erreur,onChange,value}:CustomInputProps)=>{
 
-  const EmailInput = ({placeholder}:any)=>{
     const [focus,setFocus] = useState<boolean>(false)
-    const [inputValue,SetInputValue] = useState<string>('')
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-
-    const handleChange =(e:ChangeEvent<HTMLInputElement>)=>{
-        SetInputValue(e.target.value)
-    }
-    const handleFocus =()=>{
-        setFocus(true)  
-    }
-    const handleBlur=()=>{
-        if (inputValue==="") {
-            setFocus(false)
-        }
-        else if (inputValue ) {
-            
-        }
-    }
-    const clickLabel = ()=>{
-        if (focus && inputValue==="") {
-            setFocus(false)
-        }
-        else {
-            setFocus(true)
-        }
-    }
-    return (
-        <>
-             <div className="relative mt-10">
-                <h3 className={`text-primary text-2xl absolute left-5 ${focus? 'top-[-1.5vh] text-2xl' : ' text-3xl top-3'} bg-white`} id="label" onClick={clickLabel}>{placeholder}</h3>
-                <input type="email" className="p-4 text-2xl w-[30vw] border-2 border-primary rounded-lg" onFocus={handleFocus} onBlur={handleBlur} value={inputValue} onChange={handleChange}/>
-            </div>
-        </>
-    )
-  }
-  const PswdInput = ({placeholder}:any)=>{
-    const [focus,setFocus] = useState<boolean>(false)
-    const [inputValue,SetInputValue] = useState<string>('')
-
-
-    const handleChange =(e:ChangeEvent<HTMLInputElement>)=>{
-        SetInputValue(e.target.value)
-    }
+    const [inputValue,SetInputValue] = useState<string>()
     const handleFocus =()=>{
         setFocus(true)  
     }
@@ -139,11 +160,15 @@ export default function FormulaireInsertionUser() {
         }
     }
 
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        onChange(name, e.target.value);
+      };
+
     return (
         <>
              <div className="relative mt-10">
-                <h3 className={`text-primary text-2xl absolute left-5 ${focus? 'top-[-1.5vh] text-2xl' : ' text-3xl top-3'} bg-white`} id="label" onClick={clickLabel}>{placeholder}</h3>
-                <input type="password" className="text-3xl p-4 w-[30vw] border-2 border-primary rounded-lg" onFocus={handleFocus} onBlur={handleBlur} value={inputValue} onChange={handleChange}/>
+                <h3 className={`text-2xl absolute left-5 ${focus? 'top-[-1.5vh] text-2xl' : ' text-3xl top-3'} ${erreur? 'text-red-600':'text-primary'} bg-white`} id="label" onClick={clickLabel}>{placeholder}</h3>
+                <input type={type} className={`text-3xl p-4 w-[30vw] border-2  text-black80 rounded-lg ${erreur? 'border-red-600':'border-primary'}`} onFocus={handleFocus} onBlur={handleBlur} name={name} onChange={handleChange} value={value}/>
             </div>
         </>
     )
