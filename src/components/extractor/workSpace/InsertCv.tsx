be335df41c5d70178f7a65fa23c62be624c98f71
fandresena <fandresenaantokiniaina1@gmail.com>
2024-05-29 18:56:27 +0200

    fonctionnalité workspace extracteur 45% fini

--- src/components/extractor/workSpace/InsertCv.tsx
+++ src/components/extractor/workSpace/InsertCv.tsx
@@ -1,35 +1,147 @@
-import {useRef } from "react";
+import axios from "axios";
+import {ChangeEvent, ChangeEventHandler, EventHandler, FormEvent, useRef, useState } from "react";
 
+interface dataformT {
+        first_name:string;
+        last_name:string;
+        email: string;
+        diploma: string;
+        phone_number:string;
+        source: "Apec"|"Monster"|"Pôle"|"Employe";
+        adress:string;
+        ville:string;   
+}
 const InsertCVForm = () => {
 
+    const [emailExist,setEmailExist]=useState<boolean>()
+    const [connexErr,setConnexErr]=useState<boolean>()
+    const [formValid,setFormValid]=useState<boolean>(false)
+    const [formValues,setFormValues]=useState<dataformT>({
+        first_name:'',
+        last_name:'',
+        email:"",
+        diploma:"",
+        phone_number:"",
+        source:"Apec",
+        adress:"",
+        ville:""
+    })
+
+
+    const emailValid  = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; 
+
     const formulaire = useRef <HTMLFormElement |null >(null)
+    const inputEmail = useRef <HTMLInputElement |null >(null)
+
+
+    const checkEmail = async (email:string)=>{
+        try {
+            const res = await axios.post('/extract/checkemail',{"email":email})
+            console.log(res.data);
+            
+            if (res.data.isExist===false) {
+                console.log('emailValid');
+            }
+            else  if (res.data.isExist===true) {
+                setEmailExist(true)
+                setTimeout(()=>{
+                    setEmailExist(false)
+                },2000)
+                // inputEmail.current.value="";
+                console.log('email efa ao');
+            }
+        } catch (error) {
+            setConnexErr(true)
+            setTimeout(()=>{
+                setConnexErr(false)
+            },5000)
+            console.log(error);
+            
+        }
+    }
+
+    const sendData= async (data:dataformT)=>{
+        try {
+            const res = (await axios.post('/extract/add-candidate',data))
+            if (res.status ==200) {
+                console.log('insert successfully');
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
     const handleSublit =(e:React.FormEvent<HTMLFormElement>)=>{
         e.preventDefault()
-        const formData = new FormData(formulaire.current  as HTMLFormElement)
-
-        formData.forEach((value, key) => {
-            console.log(`${key}: ${value}`);
-          });
+       sendData(formValues);
     }
+
+    inputEmail.current?.addEventListener('keydown',(e)=>{
+        // e.preventDefault()
+        if (inputEmail.current && e.key ==='Enter') {
+            if (emailValid.test(inputEmail.current.value)) {
+                checkEmail(inputEmail.current.value)
+                // console.log('email:',inputEmail.current.value);  
+            }
+        }
+    })
+
+
+    const handleChange =(e:React.ChangeEvent<HTMLInputElement>)=>{
+        setFormValues((prevValues) => {
+            const newValues = { ...prevValues, [e.target.name]: e.target.value};
+            // console.log(newValues);
+            const isValid = Object.values(newValues).every((val) => val.trim() !== '');
+            
+            // Vérifiez si tous les champs ne sont pas vides
+            setFormValid(isValid)
+
+            return newValues
+    })
+
+
+    }
+
+
     return (
       <>
-            <div className="w-[85vw] bg-white px-[2vw] pt-20 pb-10 mx-auto rounded-2xl">
-        
+            <div className="w-[85vw] bg-white px-[2vw] pt-20 pb-10 mx-auto rounded-2xl relative">
+            {
+                emailExist? <div className="absolute h-fit inset-0 flex justify-center top-2"><div className="absolute bg-red-700 rounded-lg py-3 px-[2vw] w-fit"><h1 className="text-white text-2xl text-center font-bold"><img src="/src/assets/icon/warning.svg" alt="error" className="w-8 inline-block mr-3 mb-2 text-green-500" />Email deja dans la Base</h1></div></div>:''
+            }
+             {
+                connexErr? <div className="absolute h-fit inset-0 flex justify-center top-2"><div className="absolute bg-red-700 rounded-lg py-3 px-[2vw] w-fit"><h1 className="text-white text-2xl text-center font-bold"><img src="/src/assets/icon/warning.svg" alt="error" className="w-8 inline-block mr-3 mb-2 text-green-500" />Erreur de la connexion, veuillez réssayer</h1></div></div>:''
+            }
+           
                 <form action=""onSubmit={handleSublit} ref={formulaire}>
-                    <input type="email" placeholder="email" name="email" className="w-full text-2xl border border-black90 p-3"/>
+
+                    <input type="email" placeholder="email" name="email" className={`w-full text-2xl border p-3 ${emailExist?'border-red-600':'border-black90'}`} onChange={handleChange} ref={inputEmail}/>
+                    
                     <div className="flex justify-between mt-4">
-                        <input type="text" placeholder="Nom" name="firstName" className="w-[40vw] text-2xl border border-black90 p-3"/>
-                        <input type="text" placeholder="Prenom" name="lastName" className="w-[40vw] text-2xl border border-black90 p-3"/>
+                        <input type="text" placeholder="Nom" name="first_name" className="w-[40vw] text-2xl border border-black90 p-3"onChange={handleChange}/>
+                        <input type="text" placeholder="Prenom" name="last_name" className="w-[40vw] text-2xl border border-black90 p-3"onChange={handleChange}/>
                     </div>
-                    <input type="text" placeholder="Adress" name="adress" className="w-full text-2xl p-3 border border-black90 mt-3"/>
+
+                    <div className="flex justify-between">
+                        <input type="text" placeholder="Adress" name="adress" className="w-[40vw] text-2xl p-3 border border-black90 mt-3 rounded-lg"onChange={handleChange}/>
+                        <div className="mt-3">
+                                <select className="text-2xl text-black80 bg-white font-medium p-3 w-[40vw] border border-black90 rounded-lg hover:cursor-pointer" name="source" onChange={handleChange}>
+                                    <option className="text-primary hover:cursor-pointer mt-5" value="Appec"> Appec</option>
+                                    <option className="text-secondary hover:cursor-pointer mt-5" value="Monster">Monster</option>
+                                    <option className="text-extracteur hover:cursor-pointer mt-5" value="Pole_Employe">Pole employe</option>
+                                </select>
+                        </div>
+                    </div>
+                    
                     <div className="flex justify-between mt-4">
-                        <input type="text" placeholder="Num" name="tel" className="w-[40vw] text-2xl border border-black90 p-3"/>
-                        <input type="text" placeholder="Ville" name="city" className="w-[40vw] text-2xl border border-black90 p-3"/>
+                        <input type="text" placeholder="Num" name="phone_number" className="w-[40vw] text-2xl border border-black90 p-3"onChange={handleChange}/>
+                        <input type="text" placeholder="Ville" name="ville" className="w-[40vw] text-2xl border border-black90 p-3"onChange={handleChange}/>
                     </div>
-                    <input type="text" placeholder="Diplome/Poste" name="degree" className="w-full text-2xl p-3 border border-black90 mt-4"/>
+
+                    <input type="text" placeholder="Diplome/Poste" name="diploma" className="w-full text-2xl p-3 border border-black90 mt-4"onChange={handleChange}/>
+                    
                     <div className="flex justify-center">
-                        <button type="submit" className="py-4 px-20 text-2xl text-white bg-[#3BB53B] mt-14 rounded-xl">Valider</button>
+                        <button type="submit" className="py-4 px-20 text-2xl text-white bg-[#3BB53B] mt-14 rounded-xl disabled:bg-gray-400 disabled:cursor-not-allowed" disabled={formValid?false:true} >Valider</button>
                     </div>
 
                 </form>
--- src/components/extractor/workSpace/insertEmail.tsx
+++ src/components/extractor/workSpace/insertEmail.tsx
@@ -2,7 +2,10 @@ import { useEffect, useRef, useState } from "react";
 
 const InsertEmail = () => {
   const [tabList, setTablist] = useState<number[]>([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
+  const [emailExist,setEmailExist]=useState<boolean>(false)
   const liste = useRef<HTMLDivElement>(null);
+
+  const emailValid  = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; 
 
   const handleScroll = () => {
     if (liste.current) {
@@ -30,6 +33,34 @@ const InsertEmail = () => {
     }
   }, []);
 
+  const checkEmail =(email:string)=>{
+      console.log(email);
+      //verfication et insertion email dans la base
+  }
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      const email = e.currentTarget.value;
+      if (emailValid.test(email)) {
+        checkEmail(email);
+      }
+    }
+  }
+
+/*   inputEmail.current?.addEventListener('keydown',(e)=>{
+    console.log(e.target);
+      
+    // e.preventDefault()
+    if (inputEmail.current && e.key ==='Enter') {
+      
+        // if (emailValid.test(inputEmail.current.value)) {
+        //     checkEmail(inputEmail.current.value)
+        //     // console.log('email:',inputEmail.current.value);  
+        // }
+    }
+}) */
+
+
   return (
     <>
       <div className="bg-white mx-auto mt-20 w-[85vw] h-[85vh] pl-10 rounded-2xl overflow-y-scroll" ref={liste}>
@@ -40,10 +71,10 @@ const InsertEmail = () => {
         <div className="w-[80vw] border border-black20 mt-2"></div>
 
         {tabList.map((tab: number) => (
-          <div key={tab}>
+          <div key={tab} className={`${emailExist?'bg-[rgba(255,0,0,0.8)]':''}`}>
             <div className="mt-[1vh] flex justify-start">
               <h1 className="text-black90 w-[2vw] text-center text-3xl font-semibold mr-[10vw]">{tab}</h1>
-              <input type="email" className="border-none h-10 w-[40vw] py-6 text-2xl" />
+              <input type="email" className="border-none h-10 w-[40vw] py-6 text-2xl bg-transparent" onKeyDown={handleKeyDown}/>
               <button className="text-white text-xl font-semibold bg-primary px-6 rounded-xl ml-[18vw]">Edit</button>
             </div>
             <div className="w-[80vw] border border-black20 mt-2"></div>
